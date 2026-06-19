4114a10b8dc113f303fe8d25bd09f7fc3d3217a5
DJABIA KASSI VINCENT ANDRE <vincentandredev@gmail.com>
2026-06-19 21:16:54 +0200

    Avancer Correction

--- backend/attendances/admin/admin.py
+++ backend/attendances/admin/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from backend.attendances.models import Attendance
 
-# Register your models here.
+@admin.register(Attendance)
+class AttendancesAdmin(admin.ModelAdmin):
+    list_display = ("utilisateur","classe","date","statut","justifie")
+    list_filter = ("statut","justifie","date","classe")
+    # search_fields = ("utilisateur__username","motif") #permet la liaison 
